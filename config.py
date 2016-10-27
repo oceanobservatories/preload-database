211a16177518b79f2c131cddb89c30e9317089fd
Rene Gelinas <rene.p.gelinas@gmail.com>
2016-10-27 16:14:18 -0400

    Issue #11382 - Split parse_preload.py into parse_preload.py and load_preload.py
    Change-Id: Iddeba767f870f4aca3da97a4a1e92608505c050a

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ import os
 
 __basedir = os.path.abspath(os.path.dirname(__file__))
 PRELOAD_DATABASE_SQLITE_FILE_PATH = os.path.join(__basedir, "preload.db")
-PRELOAD_DATABASE_SQLITE_FILE_URI = "sqlite:///" + PRELOAD_DATABASE_SQLITE_FILE_PATH
-PRELOAD_DATABASE_SCRIPT_FILE_PATH = os.path.join(__basedir, "preload_database.sql")
+PRELOAD_DATABASE_SQLITE_FILE_URI = "sqlite:///" + \
+                                   PRELOAD_DATABASE_SQLITE_FILE_PATH
+PRELOAD_DATABASE_SCRIPT_FILE_PATH = os.path.join(__basedir,
+                                                 "preload_database.sql")
 
 SPREADSHEET_KEY = '1jIiBKpVRBMU5Hb1DJqyR16XCkiX-CuTsn1Z1VnlRV4I'
 USE_CACHED_SPREADSHEET = False
