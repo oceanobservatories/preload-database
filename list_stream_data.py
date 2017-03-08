a235c3d5102b44ebbf24733e9e0bc9a60a54c219
Peter Cable <petercable@gmail.com>
2017-03-07 17:41:54 -0800

    print all possible params where multiple options exist
    Change-Id: I672801ac16b698f09f61044f6a1cd3130739d877

--- list_stream_data.py
+++ list_stream_data.py
@@ -13,15 +13,20 @@ Usage:
     In parameter mode, it will display parameter names and calibration
     coefficients as described above for the parameter(s) provided.
 """
-
-import sys
+from collections import namedtuple
 
 import docopt
-from ooi_data.postgres.model import Stream, Parameter, MetadataBase
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
 
+from ooi_data.postgres.model import Stream, Parameter, MetadataBase
 from database import create_engine_from_url, create_scoped_session
+
+
+indent = '    '
+max_indent = 6
+param_format = '{0.name:35} {0.parameter_type:15} {0.value_encoding:10} {0.unit:30}'
+Header = namedtuple('Header', ['name', 'parameter_type', 'value_encoding', 'unit'])
+header = Header('name', 'type', 'encoding', 'units')
+header2 = Header('-' * 35, '-' * 15, '-' * 10, '-' * 30)
 
 
 def get_object_from_preload(klass, field):
@@ -31,41 +36,67 @@ def get_object_from_preload(klass, field):
         return klass.query.filter(klass.name == field).first()
 
 
+def print_parameter(parameter, indent_level, stream=None):
+    print indent * indent_level + 'PD{0:<4}'.format(parameter.id) + \
+          indent * (max_indent - indent_level) + param_format.format(parameter),
+    if stream is None:
+        print
+    else:
+        print indent + 'FROM: ' + stream.name
+
+
 def list_parameter_needs(parameter, indent_level):
     for needs_cc in parameter.needs_cc:
-        sys.stdout.write("\t" * indent_level)
-        sys.stdout.write("%s\n" % needs_cc)
+        print indent * indent_level + 'CC ' + needs_cc
 
-    for needs_parameter in parameter.needs:
-        sys.stdout.write("\t" * indent_level)
-        needs_param = needs_parameter[1][0]
-        sys.stdout.write("PD%s:\t%s:\t%s:\t%s:\t%s\n" % (needs_param.id, needs_param.name, needs_param.parameter_type,
-                                                         needs_param.value_encoding, needs_param.unit))
-        list_parameter_needs(needs_param, indent_level+1)
+    for stream, poss_params in parameter.needs:
+        if stream and len(poss_params) == 1:
+            needs_param = poss_params[0]
+            print_parameter(needs_param, indent_level, stream)
+            list_parameter_needs(needs_param, indent_level + 1)
+        elif len(poss_params) == 1:
+            needs_param = poss_params[0]
+            print_parameter(needs_param, indent_level)
+            list_parameter_needs(needs_param, indent_level + 1)
+        else:
+            print 'POSS PARAMS:'
+            for needs_param in poss_params:
+                print_parameter(needs_param, indent_level + 1)
+
+
+def list_parameter_streams(parameter):
+    print
+    print 'Streams:'
+    print
+    for stream in parameter.streams:
+        print indent + stream.name
 
 
 def list_stream_parameters(stream_names, streams):
     for index, stream in enumerate(streams):
         if stream is not None:
-            print "\nStream: DICT%s: %s " % (stream.id, stream.name)
+            print
+            print "Stream: DICT%s: %s " % (stream.id, stream.name)
+            print indent * (max_indent + 1) + '  ' + param_format.format(header)
+            print indent * (max_indent + 1) + '  ' + param_format.format(header2)
             for parameter in streams[index].parameters:
-                print "\tPD%s:\t%s:\t%s:\t%s:\t%s" % (parameter.id, parameter.name, parameter.parameter_type,
-                                                      parameter.value_encoding, parameter.unit)
+                print_parameter(parameter, 1)
                 list_parameter_needs(parameter, 2)
         else:
-            print "\nError: Stream %s was not found in " \
-                  "the preloaded database." % stream_names[index]
+            print
+            print "Error: Stream %s was not found in the preloaded database." % stream_names[index]
 
 
 def list_parameters(parameter_names, parameters):
     for index, parameter in enumerate(parameters):
         if parameter is not None:
-            print "\nParameter: PD%s: %s: %s: %s: %s" % (parameter.id, parameter.name, parameter.parameter_type,
-                                                         parameter.value_encoding, parameter.unit)
+            print
+            print_parameter(parameter, 0)
             list_parameter_needs(parameter, 1)
+            list_parameter_streams(parameter)
         else:
-            print "\nError: Parameter %s was not found in " \
-                  "the preloaded database." % parameter_names[index]
+            print
+            print "Error: Parameter %s was not found in the preloaded database." % parameter_names[index]
 
 
 def main():
@@ -78,13 +109,12 @@ def main():
     MetadataBase.query = session.query_property()
 
     if stream:
-        streams = [get_object_from_preload(Stream, f)
-                   for f in name_or_ids]
+        streams = [get_object_from_preload(Stream, f) for f in name_or_ids]
         list_stream_parameters(name_or_ids, streams)
     else:
-        parameters = [get_object_from_preload(Parameter, f)
-                      for f in name_or_ids]
+        parameters = [get_object_from_preload(Parameter, f) for f in name_or_ids]
         list_parameters(name_or_ids, parameters)
+
 
 if __name__ == '__main__':
     main()
