9cccb1dc051ccc2c852093948d176bd63bb7506a
Peter Cable <petercable@gmail.com>
2017-04-21 10:33:32 -0700

    More updates to resolve_stream.py
    Change-Id: Ife2ef4c7bb4426e369cf35a66a50785caa02a2eb

--- resolve_stream.py
+++ resolve_stream.py
@@ -2,6 +2,7 @@
 """
 Usage:
     resolve_stream.py <refdes> <method> <stream>
+    resolve_stream.py
 """
 
 import yaml
@@ -28,6 +29,11 @@ def stream_exists(refdes, method, stream, stream_map):
     return stream in stream_map.get(refdes, {}).get(method, set())
 
 
+def has_functions(stream):
+    s = Stream.query.filter(Stream.name == stream).first()
+    return bool(s.derived)
+
+
 def make_parameter_map(instruments, method, stream_map):
     parameter_map = {}
     for rd in instruments:
@@ -41,6 +47,9 @@ def make_parameter_map(instruments, method, stream_map):
 def get_collocated(refdes):
     subsite, node, sensor = refdes.split('-', 2)
     nominal_depth = NominalDepth.get_nominal_depth(subsite, node, sensor)
+    if nominal_depth is None:
+        print 'MISSING NOMINAL DEPTH RECORD: %r' % refdes
+        return set()
     if is_mobile(node):
         collocated = nominal_depth.get_colocated_node()
     else:
@@ -54,15 +63,19 @@ def get_within(refdes, meters):
         return set()
 
     nominal_depth = NominalDepth.get_nominal_depth(subsite, node, sensor)
+    if nominal_depth is None:
+        print 'MISSING NOMINAL DEPTH RECORD: %r' % refdes
+        return set()
     near = nominal_depth.get_depth_within(meters)
     return {each.reference_designator for each in near}
 
 
-def find_parameter(poss_params, poss_sources):
+def find_parameter(poss_params, poss_sources, needed_stream):
     for poss in poss_params:
         if poss.id in poss_sources:
-            rd, stream = poss_sources[poss.id].pop()
-            return rd, stream, poss
+            rd, stream = poss_sources[poss.id].copy().pop()
+            if needed_stream is None or stream == needed_stream.name:
+                return rd, stream, poss
     return None, None, None
 
 
@@ -83,6 +96,7 @@ def resolve_stream(refdes, method, stream, stream_map):
         collocated = set()
         near = set()
 
+    same = make_parameter_map([refdes], method, stream_map)
     collocated = make_parameter_map(collocated, method, stream_map)
     near = make_parameter_map(near, method, stream_map)
 
@@ -90,23 +104,30 @@ def resolve_stream(refdes, method, stream, stream_map):
         print_parameter(p, stream=stream, rd=refdes)
         if p.is_function:
             for needed_stream, poss_params in p.needs:
-                if needed_stream is None:
-                    poss_params = set(poss_params)
+                poss_params = set(poss_params)
+                if needed_stream is None or needed_stream.name == stream:
                     local = poss_params.intersection(s.parameters)
                     if local:
                         found_param = local.pop()
                         print_parameter(found_param, stream=stream, rd=refdes, indent_level=1)
                         continue
 
-                    found_rd, found_stream, poss = find_parameter(poss_params, collocated)
-                    if found_rd:
-                        print_parameter(poss, stream=found_stream, rd=found_rd, indent_level=1)
-                        continue
+                found_rd, found_stream, poss = find_parameter(poss_params, same, needed_stream)
+                if found_rd:
+                    print_parameter(poss, stream=found_stream, rd=found_rd, indent_level=1)
+                    continue
 
-                    found_rd, found_stream, poss = find_parameter(poss_params, near)
-                    if found_rd:
-                        print_parameter(poss, stream=found_stream, rd=found_rd, indent_level=1)
-                        continue
+                found_rd, found_stream, poss = find_parameter(poss_params, collocated, needed_stream)
+                if found_rd:
+                    print_parameter(poss, stream=found_stream, rd=found_rd, indent_level=1)
+                    continue
+
+                found_rd, found_stream, poss = find_parameter(poss_params, near, needed_stream)
+                if found_rd:
+                    print_parameter(poss, stream=found_stream, rd=found_rd, indent_level=1)
+                    continue
+
+                print '%sNOT FOUND: needed_stream=%r poss_params=%r' % (indent, needed_stream, poss_params)
 
 
 def main():
@@ -119,6 +140,16 @@ def main():
     refdes = options['<refdes>']
     method = options['<method>']
     stream = options['<stream>']
+
+    if not all((refdes, method, stream)):
+        for refdes in sorted(stream_map):
+            for method in sorted(stream_map[refdes]):
+                if method.startswith('bad'):
+                    continue
+                for stream in sorted(stream_map[refdes][method]):
+                    if has_functions(stream):
+                        print refdes, method, stream
+                        resolve_stream(refdes, method, stream, stream_map)
 
     if stream_exists(refdes, method, stream, stream_map):
         resolve_stream(refdes, method, stream, stream_map)
